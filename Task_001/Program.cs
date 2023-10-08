aa96f28e1d7f2e33dd944a307e8be3740b5dce3f
maksimrepkov <maksimrepkov@yandex.ru>
2023-10-08 21:16:57 +0300

    Добавил решение задачи 10из ДЗ к сем 2

---
+++ Task_001/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача № 10 из ДЗ к семинару 2. Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100 && number < 1000)
+{
+    Console.WriteLine((number / 10) % 10);
+}
+else
+{
+    Console.WriteLine("Введено нетрехзначное число");
+}
